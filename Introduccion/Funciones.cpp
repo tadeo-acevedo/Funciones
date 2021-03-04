7d5c65cf55b29c3c7bdfe37a9cc883b176e1fb7a
tadeo-acevedo <79478507+tadeo-acevedo@users.noreply.github.com>
2021-03-03 20:54:00 -0600

    Add files via upload

---
+++ Introduccion/Funciones.cpp
@@ -0,0 +1,18 @@
+//Tadeo Acevedo Jean Carlos 2AV4
+#include<iostream>
+int main()
+
+{
+ int x;
+ int y;
+ int resta;
+ std::cout<<"Introduzca El Primer Numero "; std::cin>>x;
+
+ std::cout<<"Introduzca El Segundo Numero "; std::cin>>y;
+
+ resta=x-y;
+ std::cout<<"El Resultado De La Suma Es "<<resta<<::std::endl;
+
+ return 0;
+
+}
